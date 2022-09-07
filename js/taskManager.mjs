e3c1f7a284e749fd56ea9ed4ea8ff005bd6c3b1c
Timothy Pew <mrlasers@mrlasers.com>
2022-09-07 14:44:08 -0700

    sept 7, 2022

---
+++ js/taskManager.mjs
@@ -0,0 +1,19 @@
+export class TaskManager {
+  constructor(currentId = 0) {
+    this.tasks = []
+    this.currentId = currentId
+  }
+
+  addTask(name, description, assignedTo, dueDate, status) {
+    const task = {
+      id: this.currentId++,
+      name: name,
+      description: description,
+      assignedTo: assignedTo,
+      dueDate: dueDate,
+      status: 'TODO',
+    }
+
+    this.tasks.push(task)
+  }
+}
