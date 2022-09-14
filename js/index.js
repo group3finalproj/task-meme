c9a516fe1171b04b542b402a78c1474fd8a487f3
Natalyea <natashqu@yahoo.com>
2022-09-14 09:48:17 -0700

    index new code

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 // import { TaskManager } from './taskManager.mjs'
-
-const taskManager = new TaskManager();
+//Initialize a new TaskManager with currentId set to 0
+const taskManager = new TaskManager(0);
 //console.log(taskManager.tasks);
 //taskManager.addTask('laundry', 'catogorize', 'Natasha', '9/12/2022');
 console.log(taskManager.tasks);
 
+// Select the New Task Form
+const form = document.getElementById('new-todo-form')
 
 
-
+// Add an 'onsubmit'event listener 
 function onSubmit(event) {
   event.preventDefault()
 
@@ -26,7 +28,7 @@ function onSubmit(event) {
   }
 }
 
-const form = document.getElementById('new-todo-form')
+
 
 //form.addEventListener('submit', onSubmit)
 
@@ -35,6 +37,14 @@ form.addEventListener('submit', (event) => {
   event.target.closest('.input-container')?.classList.remove('invalid')
 })
 
+//Select inputs 
+
+const name = document.getElementById("exampleFormControlInput1");
+const description = document.getElementById("exampleFormControlInput1");
+const assignedTo = documnent.getElementById("formGroupAssignedToInput");
+const dueDate = document.getElementById("datepicker");
+//validation code
+
 function isValidForm(data) {
   if (data === 'good') {
     return data
@@ -42,3 +52,11 @@ function isValidForm(data) {
     return ['assignedTo']
   }
 }
+
+//input values
+
+name = input.value.trim();
+description = input.value.trim();
+assignedTo = input.value.trim(); 
+dueDate = input.value.trim();
+// Clear the form 
